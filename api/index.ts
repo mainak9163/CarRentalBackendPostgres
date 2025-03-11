6d451822352ae346682b88e036aea52679514bad
mainak <heellomeelo@gmail.com>
2025-03-12 00:11:14 +0530

    some minor folder rearrangement

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import carRoutes from './routes/carRoutes';
-import customerRoutes from './routes/customerRoutes';
+import carRoutes from '../routes/carRoutes';
+import customerRoutes from '../routes/customerRoutes';
 import bookingRoutes from './routes/bookingRoutes';
 
 const app = express();
