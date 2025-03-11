233842bd57be93ce88e10280b628acde49679ded
mainak <heellomeelo@gmail.com>
2025-03-12 00:48:49 +0530

    corrected models

--- controllers/carController.ts
+++ controllers/carController.ts
@@ -1,14 +1,25 @@
 import { Request, Response } from 'express';
 import prisma from '../models/prisma';
 
+function sanitizeInput(input: string): string {
+  return input.replace(/\x00/g, ""); // Removes null bytes
+}
+
 export const addCar = async (req: Request, res: Response) => {
   try {
     const { model, image, registrationNumber, type, pricing } = req.body;
-    const car = await prisma.car.create({
-      data: { model, image, registrationNumber, type, pricing },
-    });
+
+    const sanitizedData = {
+      model: sanitizeInput(model),
+      image: sanitizeInput(image),
+      registrationNumber: sanitizeInput(registrationNumber),
+      type: sanitizeInput(type),
+      pricing: sanitizeInput(pricing),
+    };
+
+    const car = await prisma.car.create({ data: sanitizedData });
     res.status(201).json(car);
-  } catch (error:any) {
+  } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
 };
